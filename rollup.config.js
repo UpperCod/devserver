1e4a01425796f209ab70a06a47b458c7aaccab4d
UpperCod <mtrujillo@outlook.cl>
2020-12-19 20:29:20 -0300

    fix cjs export

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
+import renameExtensions from "@betit/rollup-plugin-rename-extensions";
 import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
+
 export default {
   input: "./src/cli.js",
   output: {
@@ -8,9 +13,18 @@ export default {
     sourcemap: true,
     banner: "#!/usr/bin/env node",
   },
+  external: Object.keys(pkg.dependencies).concat("@atomico/build"),
   plugins: [
     replace({
       "PKG.VERSION": pkg.version,
+    }),
+    resolve(),
+    commonjs(),
+    renameExtensions({
+      include: ["**/*.js"],
+      mappings: {
+        ".js": ".cjs",
+      },
     }),
   ],
 };
